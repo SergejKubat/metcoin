bc56650323edf2521ad6d691e8fb385c4d65c3df
Sergej <sergej.kubat18@gmail.com>
2021-12-10 03:50:54 +0100

    File structure, tests, requirements...

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # metcoin
 Metcoin cryptocurrency
+
+** Install all packages **
+
+```
+pip install -r requirements.txt
+```
+
+** Run the tests **
+
+```
+python -m pytest backend/tests
+```
--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,14 @@
 import time
 
-from crypto_hash import crypto_hash
+from backend.utils.crypto_hash import crypto_hash
+
+GENESIS_DATA = {
+    'timestamp': 1,
+    'last_hash': 'genesis_last_hash',
+    'hash': 'genesis_hash',
+    'data': []
+}
+
 
 class Block:
     def __init__(self, timestamp, last_hash, hash, data):
@@ -27,4 +35,4 @@ class Block:
 
     @staticmethod
     def genesis():
-        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+        return Block(GENESIS_DATA['timestamp'], GENESIS_DATA['last_hash'], GENESIS_DATA['hash'], GENESIS_DATA['data'])
