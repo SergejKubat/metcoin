df1452df5295516a2cad4cf13246af545501c55e
Sergej <sergej.kubat18@gmail.com>
2021-12-10 02:49:21 +0100

    Simple block and blockchain class

---
+++ crypto_hash.py
@@ -0,0 +1,10 @@
+import hashlib
+import json
+
+
+def crypto_hash(*args):
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+
+    joined_data = ''.join(stringified_args)
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
